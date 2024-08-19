db03362544e842277c453f3f6567ecade4690c27
Morgane Allemann <malleman@c3r2s1.42lausanne.ch>
2024-08-19 15:08:02 +0200

    parsing on process

--- srcs/Servers.cpp
+++ srcs/Servers.cpp
@@ -1,43 +1,56 @@
 #include "Servers.hpp"
 
 Servers::Servers(const std::string &conf_file){
-    std::ifstream confFile(conf_file.c_str());
-    if (!confFile.is_open()){
-        std::cout << "Error: cannot open config file." << std::endl;
-        return ;
-    }
-    else
-        loadConfig(confFile);
-    
+	std::ifstream confFile(conf_file.c_str());
+	if (!confFile.is_open()){
+		std::cout << "Error: cannot open config file." << std::endl;
+		return ;
+	}
+	else
+		loadConfig(confFile);
+	
 }
 
 Servers::Servers(Servers const &cpy){
-    *this = cpy;
+	*this = cpy;
 }
 
 Servers::~Servers(){}
 
 Servers &Servers::operator=(Servers const &rhs){
-    this->_servConf = rhs._servConf;
-    return (*this);
+	this->_servConf = rhs._servConf;
+	return (*this);
 }
 
 void    Servers::loadConfig(std::ifstream confFile){
-    std::string line;
-    std::string whitespaces(" \t\f\v\n\r");
-    bool        locationFlag = false;
-    ServerConf  currentServer;
-    Location    currentLocation;
+	std::string line;
+	std::string whitespaces(" \t\f\v\n\r");
+	bool        locationFlag = false;
+	ServerConf  currentServer;
+	Location    currentLocation;
 
-    while (std::getline(confFile, line)){
-        line.erase(0, line.find_first_not_of(whitespaces));
-        line.erase(line.find_last_not_of(whitespaces) + 1);
+	while (std::getline(confFile, line)){
+		line.erase(0, line.find_first_not_of(whitespaces));
+		line.erase(line.find_last_not_of(whitespaces) + 1);
 
-        if (line.empty() || line[0] == '#')
-            continue;
-    }
-    if (line.find("server {")){
-        locationFlag = false;
-        currentServer = ServerConf();
-    }
+		if (line.empty() || line[0] == '#')
+			continue;
+		if (line.find("server {")){
+			locationFlag = false;
+			currentServer = ServerConf();
+		}
+		else if (line.find("}")){
+			if (locationFlag){
+				currentServer._location[currentLocation._path] = currentLocation;
+				locationFlag = false;
+			}
+			else
+				this->_servConf.push_back(currentServer);
+		}
+		if (line.find("location ")){
+			locationFlag = true;
+			currentLocation = Location();
+
+		}
+
 }
